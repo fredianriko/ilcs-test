3ac0e8fba14432436b8f6cb8264b7342a83424a5
Fredi Anriko <fredianriko@gmail.com>
2021-10-01 18:24:03 +0700

    add changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://prodev-api.ilcs.co.id/ibis_api_external_dev_v2/index.php/SingleBilling/getSppBDoc").then((response) => {
+      setData(response.data);
+      console.log(response.data);
+    });
+  }, []);
+
+  return data.forEach((items) => (
+    <table>
+      <th>{items[0]}</th>
+      <th>{items[1]}</th>
+    </table>
+  ));
 }
 
 export default App;
