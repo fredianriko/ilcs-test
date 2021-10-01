f5dcbefc99d228072affab0c7684f7858b157c9c
Fredi Anriko <fredianriko@gmail.com>
2021-10-01 19:20:43 +0700

    finishing

--- src/App.js
+++ src/App.js
@@ -4,20 +4,46 @@ import axios from "axios";
 
 function App() {
   const [data, setData] = useState([]);
+  const [url, setUrl] = useState("https://prodev-api.ilcs.co.id/ibis_api_external_dev_v2/index.php/SingleBilling/getSppBDoc");
 
   useEffect(() => {
-    axios.get("https://prodev-api.ilcs.co.id/ibis_api_external_dev_v2/index.php/SingleBilling/getSppBDoc").then((response) => {
-      setData(response.data);
-      console.log(response.data);
-    });
-  }, []);
+    const fetchData = async () => {
+      const result = await axios(url);
+      setData(result.data.data);
+      // console.log(result.data.data);
+    };
+    fetchData();
+  }, [url]);
 
-  return data.forEach((items) => (
+  const dataRekap = data.map((items) => {
+    // const loop = () => {
+    //   for (let i = 0; i <= items.length; i++) {
+    //     return i;
+    //     console.log(i);
+    //   }
+    // };
+    return (
+      <tr className="container">
+        <td className="data">{data.indexOf(items)}</td>
+        <td className="data">{items.ID_DOKUMEN}</td>
+        <td className="data">{items.NAMA_DOKUMEN}</td>
+      </tr>
+    );
+  });
+  // const dataDokumen = data.map((items) => console.log(items));
+
+  return (
     <table>
-      <th>{items[0]}</th>
-      <th>{items[1]}</th>
+      <thead>
+        <tr>
+          <td className="header">NO</td>
+          <td className="header">ID DOKUMEN</td>
+          <td className="header">NAMA DOKUMEN</td>
+        </tr>
+      </thead>
+      <tbody>{dataRekap}</tbody>
     </table>
-  ));
+  );
 }
 
 export default App;
